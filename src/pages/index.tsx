38d14f5e46f30e69a7c24e93232406d5cb5b4cd8
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2021-02-26 18:38:44 -0300

    cookies, modal levelup, no deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 
 import { ExperienceBar } from '../components/ExperienceBar'
 import { CompletedChallenges } from '../components/CompletedChallenges'
@@ -7,31 +8,61 @@ import { Profile } from '../components/Profile'
 import { ChallengeBox } from '../components/ChallengeBox'
 
 import { CountdownProvider } from '../contexts/CountdownContext'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
 import styles from '../styles/pages/Home.module.css'
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
 
-      <ExperienceBar/>
+        <ExperienceBar/>
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
 
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // Roda apenas no servidor, nao no browser
+  // Roda antes da tela terminar de renderizar
+  // Permite que crawlers visualizem dados antes da tela
+  // chamada api
+
+  const { level, currentExperience, challengesCompleted } = context.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
